25b18a1f80e3654cbd70dca3c1eed42ecfaa1d59
Laurens van der Drift <Lauwy222@hotmail.com>
2021-11-17 14:03:47 +0100

    Main Code

---
+++ Source.c
@@ -0,0 +1,25 @@
+#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char scanned[60];
+
+	printf("Geef een woord: ");
+	scanf("%s", &scanned);
+	int length = strlen(scanned);
+
+	printf("\n\nThe word: %s has %d characters ", scanned, length);
+	
+	char* normal;
+	normal = scanned;
+
+	printf("and is reversed: ");
+
+	for (int i = 0; i < length; ++i) {
+		int last = length - i - 1;
+		printf("%c", *(normal + last));
+	}
+	
+	return 0;
+}
